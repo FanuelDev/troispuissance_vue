f1b7dba4ac59d75e0ca565dae69493ad63fdc43a
Fanuel Israel TOVEY <fanueldev2.0@gmail.com>
2026-02-12 09:48:26 +0000

    Update notes.vue

--- src/views/notes.vue
+++ src/views/notes.vue
@@ -1,6 +1,97 @@
 <template>
-<!--  <div class="banner1"></div>-->
-<section class="section-banner-note1">
+  <div class="banner1"></div>
+
+    <section class="top container">
+      <div class="row">
+        <div class="col-md-12 p-4">
+          <div class="text-center border-pri" data-aos="fade-up">
+            <h1 class="title text-primary fw-bold">NOTES</h1>
+          </div>
+          <div class="py-4 border-pri" data-aos="fade-up">
+            <p class="text-center">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque eum accusamus aliquid
+              voluptates? Sint ab deserunt autem omnis, tenetur accusantium animi labore, enim quidem
+              unde fugit ipsum fugiat voluptatum iure! Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Aut, placeat? Dolorum est id eum totam. Lorem ipsum dolor sit amet
+              consectetur adipisicing elit. Eum qui vitae nobis! Quasi exercitationem laboriosam eum,
+              qui ipsam aliquam rem vel nam culpa maiores ipsum.
+            </p>
+          </div>
+          <div class="text-center">
+            <div class="container">
+              <div class="row">
+                <div class="col-md-6 offset-md-3 my-4">
+                  <h3 class="title text-primary mt-4" data-aos="fade-up">
+                    FAITES VOTRE CHOIX PAR TYPE DE BOISSON
+                  </h3>
+                  <div class="my-5 d-flex justify-content-between" data-aos="fade-up">
+                    <button class="btn btn-primary">Vins</button>
+                    <button class="btn btn-primary">Spiritueux</button>
+                  </div>
+                </div>
+
+                <div class="input-group col-md-12 my-4" data-aos="fade-up">
+                  <span class="input-group-text s2">
+                    <img
+                      src="/src/assets/icons/s2.png"
+                      alt="Icon"
+                      style="width: 20px; height: 20px"
+                    />
+                  </span>
+                  <input type="search" placeholder="Recherche ..." class="form-control input1" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="my-5 text-center">
+            <h3 class="title text-primary mt-4" data-aos="fade-up">LAISSEZ-VOUS GUIDER</h3>
+          </div>
+          <div class="bg-primary p-5">
+            <div class="container">
+              <div class="row align-item-center">
+                <div class="col-md-6">
+                  <h5 class="text-white title" data-aos="fade-up">Recherche de vin</h5>
+                  <p class="text-white" data-aos="fade-up">
+                    Dis-mois qui tu est et je te dirai quel vin il te faut
+                  </p>
+                  <button class="btn btn-outline-light" data-aos="fade-up">Traiter</button>
+                </div>
+                <div class="col-md-6">
+                  <form action="#">
+                    <div class="container">
+                      <div class="row">
+                        <div class="col-md-6 my-2">
+                          <input
+                            type="text"
+                            class="form-control input2"
+                            placeholder="Je recherche une bouteille"
+                          />
+                        </div>
+                        <div class="col-md-6 my-2">
+                          <input type="text" class="form-control input2" placeholder="Millesime" />
+                        </div>
+                        <div class="col-md-6 my-2">
+                          <input type="text" class="form-control input2" placeholder="d'origine" />
+                        </div>
+                        <div class="col-md-6 my-2">
+                          <input
+                            type="text"
+                            class="form-control input2"
+                            placeholder="j'ai un budget de"
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+<section class="section-banner-note1 bg-light pb-5 mt-5 pt-4">
   <div class="banner-note1">
     <div class="text-center top" data-aos="fade-up">
       <h1 class="hh1 text-primary fw-bold">Explorer les resultats <br> par distinction</h1>
@@ -11,7 +102,7 @@
       </p>
     </div>
 
-    <div class="top d-flex align-items-center">
+    <div class="py-5 d-flex align-items-center">
       <div class="container">
         <div class="row justify-content-center g-4">
           <div class="col-md-8 col-offset-2">
@@ -78,95 +169,7 @@
   </section>
 
 
-  <!--  <section class="top container">-->
-<!--    <div class="row">-->
-<!--      <div class="col-md-12 p-4">-->
-<!--        <div class="text-center border-pri" data-aos="fade-up">-->
-<!--          <h1 class="title text-primary fw-bold">NOTES</h1>-->
-<!--        </div>-->
-<!--        <div class="py-4 border-pri" data-aos="fade-up">-->
-<!--          <p class="text-center">-->
-<!--            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque eum accusamus aliquid-->
-<!--            voluptates? Sint ab deserunt autem omnis, tenetur accusantium animi labore, enim quidem-->
-<!--            unde fugit ipsum fugiat voluptatum iure! Lorem ipsum dolor sit amet consectetur-->
-<!--            adipisicing elit. Aut, placeat? Dolorum est id eum totam. Lorem ipsum dolor sit amet-->
-<!--            consectetur adipisicing elit. Eum qui vitae nobis! Quasi exercitationem laboriosam eum,-->
-<!--            qui ipsam aliquam rem vel nam culpa maiores ipsum.-->
-<!--          </p>-->
-<!--        </div>-->
-<!--        <div class="text-center">-->
-<!--          <div class="container">-->
-<!--            <div class="row">-->
-<!--              <div class="col-md-6 offset-md-3 my-4">-->
-<!--                <h3 class="title text-primary mt-4" data-aos="fade-up">-->
-<!--                  FAITES VOTRE CHOIX PAR TYPE DE BOISSON-->
-<!--                </h3>-->
-<!--                <div class="my-5 d-flex justify-content-between" data-aos="fade-up">-->
-<!--                  <button class="btn btn-primary">Vins</button>-->
-<!--                  <button class="btn btn-primary">Spiritueux</button>-->
-<!--                </div>-->
-<!--              </div>-->
 
-<!--              <div class="input-group col-md-12 my-4" data-aos="fade-up">-->
-<!--                <span class="input-group-text s2">-->
-<!--                  <img-->
-<!--                    src="/src/assets/icons/s2.png"-->
-<!--                    alt="Icon"-->
-<!--                    style="width: 20px; height: 20px"-->
-<!--                  />-->
-<!--                </span>-->
-<!--                <input type="search" placeholder="Recherche ..." class="form-control input1" />-->
-<!--              </div>-->
-<!--            </div>-->
-<!--          </div>-->
-<!--        </div>-->
-<!--        <div class="my-5 text-center">-->
-<!--          <h3 class="title text-primary mt-4" data-aos="fade-up">LAISSEZ-VOUS GUIDER</h3>-->
-<!--        </div>-->
-<!--        <div class="bg-primary p-5">-->
-<!--          <div class="container">-->
-<!--            <div class="row align-item-center">-->
-<!--              <div class="col-md-6">-->
-<!--                <h5 class="text-white title" data-aos="fade-up">Recherche de vin</h5>-->
-<!--                <p class="text-white" data-aos="fade-up">-->
-<!--                  Dis-mois qui tu est et je te dirai quel vin il te faut-->
-<!--                </p>-->
-<!--                <button class="btn btn-outline-light" data-aos="fade-up">Traiter</button>-->
-<!--              </div>-->
-<!--              <div class="col-md-6">-->
-<!--                <form action="#">-->
-<!--                  <div class="container">-->
-<!--                    <div class="row">-->
-<!--                      <div class="col-md-6 my-2">-->
-<!--                        <input-->
-<!--                          type="text"-->
-<!--                          class="form-control input2"-->
-<!--                          placeholder="Je recherche une bouteille"-->
-<!--                        />-->
-<!--                      </div>-->
-<!--                      <div class="col-md-6 my-2">-->
-<!--                        <input type="text" class="form-control input2" placeholder="Millesime" />-->
-<!--                      </div>-->
-<!--                      <div class="col-md-6 my-2">-->
-<!--                        <input type="text" class="form-control input2" placeholder="d'origine" />-->
-<!--                      </div>-->
-<!--                      <div class="col-md-6 my-2">-->
-<!--                        <input-->
-<!--                          type="text"-->
-<!--                          class="form-control input2"-->
-<!--                          placeholder="j'ai un budget de"-->
-<!--                        />-->
-<!--                      </div>-->
-<!--                    </div>-->
-<!--                  </div>-->
-<!--                </form>-->
-<!--              </div>-->
-<!--            </div>-->
-<!--          </div>-->
-<!--        </div>-->
-<!--      </div>-->
-<!--    </div>-->
-<!--  </section>-->
 
 <!--  <section data-aos="fade-up" class="top">-->
 <!--    <div class="container">-->
@@ -214,25 +217,9 @@
 
 <style lang="css" scoped>
 .section-banner-note1 {
-  background-image: url("/src/assets/images/use/10.jpg");
-  background-position: 50% 50%;
-  background-size: cover;
-  background-repeat: no-repeat;
-  min-height: 120vh;
   position: relative;
 }
 
-.banner-note1 {
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  inset: 0;
-  background: linear-gradient(
-    to top,
-    rgba(255, 255, 255, 0.10) 5%,
-    rgba(255, 255, 255, 0.95)
-  );
-}
 
 .hh1 {
   font-size: 60px;
@@ -249,6 +236,7 @@
   background-repeat: no-repeat;
   height: 100vh;
   position: relative;
+  margin-top: 0;
 }
 
 .banner-note2 {
